c0fa64c9793fe6606d3d6cd580bfbf6b679381cd
jperez1478 <jessicperez3@gmail.com>
2022-04-08 08:17:02 -0500

    cloudkit manager catch saved records clean

--- pvCovidTrace-beta/Views/Screens/ProfileView/ProfileViewModel.swift
+++ pvCovidTrace-beta/Views/Screens/ProfileView/ProfileViewModel.swift
@@ -34,40 +34,25 @@ final class ProfileViewModel: ObservableObject {
         // Create our CKRecord from the profileView
         let profileRecord = createProfileRecord()
         
-        // Get our UserRecordID from the Container
-        CKContainer.default().fetchUserRecordID { recordID, error  in
-            guard  let recordID =  recordID, error == nil else {
-                print(error!.localizedDescription)
-                return
-            }
-            //Get UserRecord from the Public Database
-            CKContainer.default().publicCloudDatabase.fetch(withRecordID:  recordID) { userRecord, error in
-                guard let userRecord = userRecord, error == nil else {
-                    print(error!.localizedDescription)
-                    return
-                }
-                
-                // Create refrence on UserRecord to the PVProfile we created
-                userRecord["userProfile"] = CKRecord.Reference(recordID: profileRecord.recordID, action: .none)
-                
-                // Create a CKOperarion to save  User and Profile Records
-                //ARRAY of users to create
-                let operation = CKModifyRecordsOperation(recordsToSave: [userRecord, profileRecord])
-                //modifier for record
-                operation.modifyRecordsCompletionBlock = { savedRecords, _, error in
-                    guard let savedRecords = savedRecords, error == nil else {
-                        print(error!.localizedDescription)
-                        return
-                        
-                    }
-                    
-                    //if records save print to test
-                    print(savedRecords)
-                }
-                
-                CKContainer.default().publicCloudDatabase.add(operation) //similar to url session like network call
-            }
+        guard let userRecord = CloudKitManager.shared.userRecord else {
+            //show alert
             
+            return
+        }
+        
+      
+        // Create refrence on UserRecord to the PVProfile we created
+        userRecord["userProfile"] = CKRecord.Reference(recordID: profileRecord.recordID, action: .none)
+       
+        CloudKitManager.shared.batchSave(records: [userRecord, profileRecord]) { result in
+            switch result {
+            case .success(_):
+                //show laert
+                break
+                
+            case .failure(_):
+                break
+            }
         }
         
     }
@@ -76,42 +61,41 @@ final class ProfileViewModel: ObservableObject {
     
     //get user record to reference userID
     func getProfile(){
-        CKContainer.default().fetchUserRecordID { recordID, error  in
-            guard  let recordID =  recordID, error == nil else {
-                print(error!.localizedDescription)
-                return
-            }
+        guard let userRecord = CloudKitManager.shared.userRecord else {
+            //show alert
             
-            //Get UserRecord from the Public Database
-            CKContainer.default().publicCloudDatabase.fetch(withRecordID:  recordID) { userRecord, error in
-                guard let userRecord = userRecord, error == nil else {
-                    print(error!.localizedDescription)
-                    return
-                }
+            return
+        }
+        
+       guard  let profileRefrence = userRecord["userProfile"] as? CKRecord.Reference else {
+            //show alert
+            return
+       }
+        
+       let profileRecordID = profileRefrence.recordID
+        
+        CloudKitManager.shared.fetchRecord(with: profileRecordID) { result  in
+            DispatchQueue.main.async { [self] in
+            switch result {
                 
-                let  profileRefrence = userRecord["userProfile"] as! CKRecord.Reference
-                let profileRecordID = profileRefrence.recordID
+            case .success(let record):
+                //update UI
+              
+                    let profile = PVProfile(record: record)
+                    firstName = profile.firstName
+                    lastName = profile.lastName
+                    covidStatus = profile.covidStatus
                 
-                CKContainer.default().publicCloudDatabase.fetch(withRecordID: profileRecordID) { profileRecord, error in
-                    guard let profileRecord = profileRecord, error == nil else {
-                        print(error!.localizedDescription)
-                        
-                        return
-                    }
-                    
-                    //update UI
-                    DispatchQueue.main.async { [self] in 
-                        let profile = PVProfile(record: profileRecord)
-                        firstName = profile.firstName
-                        lastName = profile.lastName
-                        covidStatus = profile.covidStatus
-                    }
-                }
-                
+            case .failure(_):
+                //show alert
+                break
             }
         }
-    }
-    
+        
+        }
+}
+
+                    
     private func createProfileRecord() -> CKRecord {
         
         let profileRecord = CKRecord(recordType: RecordType.profile)
